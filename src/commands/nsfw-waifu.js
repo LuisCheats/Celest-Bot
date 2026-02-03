430c91665b6b603af4187a4cc227ce96e8f79052
LuisCheats <luiselcrack183@gmail.com>
2026-02-03 07:45:49 -0500

    update nsfw-waifu.js

--- src/commands/nsfw-waifu.js
+++ src/commands/nsfw-waifu.js
@@ -4,7 +4,7 @@ const handler = async (m, { conn, usedPrefix, command }) => {
     try {
         m.react('🕒');
         
-        const response = await fetch('https://averry-api.vercel.app/docs?share=nsfw');
+        const response = await fetch('https://averry-api.vercel.app/nsfw/nsfw1');
         if (!response.ok) throw new Error('Error en la API');
         
         const { url } = await response.json();
