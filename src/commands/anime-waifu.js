96056f36db54f17051155b9cc12c2a00a722c726
LuisCheats <luiselcrack183@gmail.com>
2026-02-17 23:14:27 -0500

    update anime-waifu.js

--- src/commands/anime-waifu.js
+++ src/commands/anime-waifu.js
@@ -4,7 +4,7 @@ const handler = async (m, { conn, usedPrefix, command }) => {
     try {
         m.react('🕒');
         
-        const response = await fetch('https://api.waifu.pics/sfw/waifu');
+        const response = await fetch('https://celest-api.vercel.app/api/random/waifu');
         if (!response.ok) throw new Error('Error en la API');
         
         const { url } = await response.json();
